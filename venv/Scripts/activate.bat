a15e072ec0fcca66fe7598fac306d2d3b39c3a29
moaz-jamil <jamilahmed20201970@gmail.com>
2026-06-01 13:19:18 +0500

    del

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=M:\Fyp-project\Smart-AI-chatbot-Automation--Fyp-Project\venv
+set VIRTUAL_ENV=D:\FYP\vendora\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
